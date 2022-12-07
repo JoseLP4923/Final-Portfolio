ec116d4d956e3c28b8fd3efd557b26dc16afe0aa
JoseLP4923 <115571595+JoseLP4923@users.noreply.github.com>
2022-12-06 23:23:38 -0600

    Add files via upload

---
+++ Problem_1.py
@@ -0,0 +1,8 @@
+#Jose Lopez
+#11/1/2022
+#
+#A program that uses a for loop to print "hello world" 100 times
+
+
+for i in range(1,101):
+    print("hello world")
---
+++ Problem_2.py
@@ -0,0 +1,9 @@
+#Jose Lopez
+#11/1/2022
+
+#A program that prints specific numbers and its square.
+#
+num_list = [12,10,32,3,66,17,42,99,20]
+for number in num_list:
+    print(number)
+    print(number ** 2)
